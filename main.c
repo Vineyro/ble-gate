7ac1a3ac511c17817d735371dd6034a81ee33fed
Vineyro <safronov2096@mail.ru>
2024-02-15 12:06:32 +0700

    update

--- main.c
+++ main.c
@@ -3,10 +3,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    
-    printf("\nHello, world!\n\n");
+
+    printf("\nHello from ble-gate\n\n");
+
+    fprintf(stderr, "Enabled: %s", argv[0]);
+    fprintf(stderr, "FTP: %s", argv[1]);
+    fprintf(stderr, "Timeout: %s", argv[2]);
     return 0;
 
 }
