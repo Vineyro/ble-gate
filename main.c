352a311cb73c4659001eaa0e5a30ab5f9f1a68e5
Vineyro <safronov2096@mail.ru>
2024-02-16 09:21:20 +0700

    Update

--- main.c
+++ main.c
@@ -8,9 +8,9 @@ int main(int argc, char *argv[])
 
     printf("\nHello from ble-gate\n\n");
 
-    fprintf(stderr, "Enabled: %s", argv[0]);
-    fprintf(stderr, "FTP: %s", argv[1]);
-    fprintf(stderr, "Timeout: %s", argv[2]);
+    //fprintf(stderr, "Enabled: %s", argv[0]);
+    //fprintf(stderr, "FTP: %s", argv[1]);
+    //fprintf(stderr, "Timeout: %s", argv[2]);
     return 0;
 
 }
