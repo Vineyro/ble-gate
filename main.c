b86d82ad150e4fb3939ef33fefda50b7ede100c4
Vineyro <safronov2096@mail.ru>
2024-02-16 12:07:17 +0700

    update

--- main.c
+++ main.c
@@ -1,16 +1,23 @@
-
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include <string.h>
+
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
 
 int main(int argc, char *argv[])
 {
 
-    printf("\nHello from ble-gate\n\n");
+    
+
     system("logger -s Hello from ble-gate");
-    //fprintf(stderr, "Enabled: %s", argv[0]);
-    //fprintf(stderr, "FTP: %s", argv[1]);
-    //fprintf(stderr, "Timeout: %s", argv[2]);
+
     return 0;
 
 }
