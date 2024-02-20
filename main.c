1756338d9e1720568e0f5048dd0343fe49332401
Vineyro <safronov2096@mail.ru>
2024-02-20 14:42:16 +0700

    u

--- main.c
+++ main.c
@@ -1,260 +1,451 @@
 /*
- * Copyright (C) 2011-2014 Felix Fietkau <nbd@openwrt.org>
+ * SPI testing utility (using spidev driver)
+ *
+ * Copyright (c) 2007  MontaVista Software, Inc.
+ * Copyright (c) 2007  Anton Vorontsov <avorontsov@ru.mvista.com>
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License version 2.1
- * as published by the Free Software Foundation
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Cross-compile with cross-gcc -I/path/to/cross-kernel/include
  */
 
+#include <stdint.h>
 #include <unistd.h>
-#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/ioctl.h>
+#include <linux/ioctl.h>
+#include <sys/stat.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
 
-#include <libubox/blobmsg_json.h>
-#include "libubus.h"
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
+/** @brief BLE address length. */
+#define BLE_GAP_ADDR_LEN (6)
+#define SWAP_16(a) a = ((a & 0x00FF) << 8) | ((a & 0xFF00) >> 8);
 
-static struct ubus_context *ctx;
-static struct ubus_subscriber test_event;
-static struct blob_buf b;
+#define SCAN_ENABLE  0x81 // Включить сканирование
+#define SCAN_DISABLE 0x82 // Отключить сканирование
+#define TABLE_READ   0x83 // Считать BLE_ID_TABLE
+#define TABLE_READ_CONTINUE 0x84 // продолжить чтение BLE_ID_TABLE
 
-enum {
-	HELLO_ID,
-	HELLO_MSG,
-	__HELLO_MAX
-};
-
-static const struct blobmsg_policy hello_policy[] = {
-	[HELLO_ID] = { .name = "id", .type = BLOBMSG_TYPE_INT32 },
-	[HELLO_MSG] = { .name = "msg", .type = BLOBMSG_TYPE_STRING },
-};
-
-struct hello_request {
-	struct ubus_request_data req;
-	struct uloop_timeout timeout;
-	int fd;
-	int idx;
-	char data[];
-};
-
-static void test_hello_fd_reply(struct uloop_timeout *t)
+#define BLE_ID_TABLE_SIZE 32
+#pragma pack ( push, 1 )
+struct my_ble_id_table_cell_s
 {
-	struct hello_request *req = container_of(t, struct hello_request, timeout);
-	char *data;
+    uint8_t addr[BLE_GAP_ADDR_LEN]; // Постоянная составляющая ускорения
+    uint8_t m_data_0_dev_type; //DEVICE_TYPE // Наши данные для оповещения
+    uint8_t m_data_1_bat_lvl;  // Заряд батареи
+    uint8_t m_data_2_rec_flag; // Флаг включения записи
+    int8_t  tx_power_level;    // Мощность передатчика
+};
 
-	data = alloca(strlen(req->data) + 32);
-	sprintf(data, "msg%d: %s\n", ++req->idx, req->data);
-	if (write(req->fd, data, strlen(data)) < 0) {
-		close(req->fd);
-		free(req);
-		return;
+struct my_ble_id_table_s
+{
+    uint16_t	size;
+    uint16_t	count;
+    struct my_ble_id_table_cell_s cells[BLE_ID_TABLE_SIZE];
+};
+#pragma pack ( pop )
+
+static void pabort(const char *s)
+{
+	perror(s);
+	abort();
+}
+
+static const char *device = "/dev/spidev1.0";
+static uint32_t mode;
+static uint8_t bits = 8;
+static uint32_t speed = 1000000;
+static uint16_t delay;
+static int verbose;
+static int iterations;
+struct my_ble_id_table_s my_ble_id_table;
+
+uint8_t default_tx[] = {
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+        0xF0, 0x0D,
+};
+
+uint8_t default_rx[ARRAY_SIZE(default_tx)] = {0, };
+char *input_tx;
+
+static void hex_dump(const void *src, size_t length, size_t line_size,
+		     char *prefix)
+{
+	int i = 0;
+	const unsigned char *address = src;
+	const unsigned char *line = address;
+	unsigned char c;
+
+	printf("%s | ", prefix);
+	while (length-- > 0) {
+		printf("%02X ", *address++);
+		if (!(++i % line_size) || (length == 0 && i % line_size)) {
+			if (length == 0) {
+				while (i++ % line_size)
+					printf("__ ");
+			}
+			printf(" | ");  /* right close */
+			while (line < address) {
+				c = *line++;
+				printf("%c", (c < 33 || c == 255) ? 0x2E : c);
+			}
+			printf("\n");
+			if (length > 0)
+				printf("%s | ", prefix);
+		}
 	}
-
-	uloop_timeout_set(&req->timeout, 1000);
 }
 
-static void test_hello_reply(struct uloop_timeout *t)
+/*
+ *  Unescape - process hexadecimal escape character
+ *      converts shell input "\x23" -> 0x23
+ */
+static int unescape(char *_dst, char *_src, size_t len)
 {
-	struct hello_request *req = container_of(t, struct hello_request, timeout);
-	int fds[2];
+	int ret = 0;
+	int match;
+	char *src = _src;
+	char *dst = _dst;
+	unsigned int ch;
 
-	blob_buf_init(&b, 0);
-	blobmsg_add_string(&b, "message", req->data);
-	ubus_send_reply(ctx, &req->req, b.head);
+	while (*src) {
+		if (*src == '\\' && *(src+1) == 'x') {
+			match = sscanf(src + 2, "%2x", &ch);
+			if (!match)
+				pabort("malformed input string");
 
-	if (pipe(fds) == -1) {
-		fprintf(stderr, "Failed to create pipe\n");
-		return;
+			src += 4;
+			*dst++ = (unsigned char)ch;
+		} else {
+			*dst++ = *src++;
+		}
+		ret++;
 	}
-	ubus_request_set_fd(ctx, &req->req, fds[0]);
-	ubus_complete_deferred_request(ctx, &req->req, 0);
-	req->fd = fds[1];
-
-	req->timeout.cb = test_hello_fd_reply;
-	test_hello_fd_reply(t);
-}
-
-static int test_hello(struct ubus_context *ctx, struct ubus_object *obj,
-		      struct ubus_request_data *req, const char *method,
-		      struct blob_attr *msg)
-{
-	struct hello_request *hreq;
-	struct blob_attr *tb[__HELLO_MAX];
-	const char format[] = "%s received a message: %s";
-	const char *msgstr = "(unknown)";
-
-	blobmsg_parse(hello_policy, ARRAY_SIZE(hello_policy), tb, blob_data(msg), blob_len(msg));
-
-	if (tb[HELLO_MSG])
-		msgstr = blobmsg_data(tb[HELLO_MSG]);
-
-	size_t len = sizeof(*hreq) + sizeof(format) + strlen(obj->name) + strlen(msgstr) + 1;
-	hreq = calloc(1, len);
-	if (!hreq)
-		return UBUS_STATUS_UNKNOWN_ERROR;
-
-	snprintf(hreq->data, len, format, obj->name, msgstr);
-	ubus_defer_request(ctx, req, &hreq->req);
-	hreq->timeout.cb = test_hello_reply;
-	uloop_timeout_set(&hreq->timeout, 1000);
-
-	return 0;
-}
-
-enum {
-	WATCH_ID,
-	WATCH_COUNTER,
-	__WATCH_MAX
-};
-
-static const struct blobmsg_policy watch_policy[__WATCH_MAX] = {
-	[WATCH_ID] = { .name = "id", .type = BLOBMSG_TYPE_INT32 },
-	[WATCH_COUNTER] = { .name = "counter", .type = BLOBMSG_TYPE_INT32 },
-};
-
-static void
-test_handle_remove(struct ubus_context *ctx, struct ubus_subscriber *s,
-                   uint32_t id)
-{
-	fprintf(stderr, "Object %08x went away\n", id);
-}
-
-static int
-test_notify(struct ubus_context *ctx, struct ubus_object *obj,
-	    struct ubus_request_data *req, const char *method,
-	    struct blob_attr *msg)
-{
-#if 0
-	char *str;
-
-	str = blobmsg_format_json(msg, true);
-	fprintf(stderr, "Received notification '%s': %s\n", method, str);
-	free(str);
-#endif
-
-	return 0;
-}
-
-static int test_watch(struct ubus_context *ctx, struct ubus_object *obj,
-		      struct ubus_request_data *req, const char *method,
-		      struct blob_attr *msg)
-{
-	struct blob_attr *tb[__WATCH_MAX];
-	int ret;
-
-	blobmsg_parse(watch_policy, __WATCH_MAX, tb, blob_data(msg), blob_len(msg));
-	if (!tb[WATCH_ID])
-		return UBUS_STATUS_INVALID_ARGUMENT;
-
-	test_event.remove_cb = test_handle_remove;
-	test_event.cb = test_notify;
-	ret = ubus_subscribe(ctx, &test_event, blobmsg_get_u32(tb[WATCH_ID]));
-	fprintf(stderr, "Watching object %08x: %s\n", blobmsg_get_u32(tb[WATCH_ID]), ubus_strerror(ret));
 	return ret;
 }
 
-enum {
-    COUNT_TO,
-    COUNT_STRING,
-    __COUNT_MAX
-};
-
-static const struct blobmsg_policy count_policy[__COUNT_MAX] = {
-    [COUNT_TO] = { .name = "to", .type = BLOBMSG_TYPE_INT32 },
-    [COUNT_STRING] = { .name = "string", .type = BLOBMSG_TYPE_STRING },
-};
-
-static int test_count(struct ubus_context *ctx, struct ubus_object *obj,
-		      struct ubus_request_data *req, const char *method,
-		      struct blob_attr *msg)
-{
-	struct blob_attr *tb[__COUNT_MAX];
-	char *s1, *s2;
-	uint32_t num;
-
-	blobmsg_parse(count_policy, __COUNT_MAX, tb, blob_data(msg), blob_len(msg));
-	if (!tb[COUNT_TO] || !tb[COUNT_STRING])
-		return UBUS_STATUS_INVALID_ARGUMENT;
-
-	num = blobmsg_get_u32(tb[COUNT_TO]);
-	s1 = blobmsg_get_string(tb[COUNT_STRING]);
-	s2 = count_to_number(num);
-	if (!s1 || !s2) {
-		free(s2);
-		return UBUS_STATUS_UNKNOWN_ERROR;
-	}
-	blob_buf_init(&b, 0);
-	blobmsg_add_u32(&b, "rc", strcmp(s1, s2));
-	ubus_send_reply(ctx, req, b.head);
-	free(s2);
-
-	return 0;
-}
-
-static const struct ubus_method test_methods[] = {
-	UBUS_METHOD("hello", test_hello, hello_policy),
-	UBUS_METHOD("watch", test_watch, watch_policy),
-	UBUS_METHOD("count", test_count, count_policy),
-};
-
-static struct ubus_object_type test_object_type =
-	UBUS_OBJECT_TYPE("test", test_methods);
-
-static struct ubus_object test_object = {
-	.name = "test",
-	.type = &test_object_type,
-	.methods = test_methods,
-	.n_methods = ARRAY_SIZE(test_methods),
-};
-
-static void server_main(void)
+static void transfer(int fd, uint8_t const *tx, uint8_t const *rx, size_t len)
 {
 	int ret;
+/*
+	fd = open(device, O_RDWR);
+	if (fd < 0)
+		pabort("can't open device");
 
-	ret = ubus_add_object(ctx, &test_object);
-	if (ret)
-		fprintf(stderr, "Failed to add object: %s\n", ubus_strerror(ret));
+	// spi mode
+	ret = ioctl(fd, SPI_IOC_WR_MODE32, &mode);
+	if (ret == -1)
+		pabort("can't set spi mode");
 
-	ret = ubus_register_subscriber(ctx, &test_event);
-	if (ret)
-		fprintf(stderr, "Failed to add watch handler: %s\n", ubus_strerror(ret));
+	ret = ioctl(fd, SPI_IOC_RD_MODE32, &mode);
+	if (ret == -1)
+		pabort("can't get spi mode");
 
-	uloop_run();
+	// bits per word
+	ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
+	if (ret == -1)
+		pabort("can't set bits per word");
+
+	ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
+	if (ret == -1)
+		pabort("can't get bits per word");
+
+	// max speed hz
+	ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+	if (ret == -1)
+		pabort("can't set max speed hz");
+
+	ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
+	if (ret == -1)
+		pabort("can't get max speed hz");
+
+	printf("spi mode: 0x%x bits per word: %d max speed: %d kHz\n", mode, bits, speed/1000);
+*/
+	struct spi_ioc_transfer tr = {
+		.tx_buf = (unsigned long)tx,
+		.rx_buf = (unsigned long)rx,
+		.len = len,
+		.delay_usecs = delay,
+		.speed_hz = speed,
+		.bits_per_word = bits,
+		.cs_change = 0,
+	};
+
+/*
+	struct spi_ioc_transfer tr = {
+		.tx_buf = (unsigned long)tx,
+		.rx_buf = (unsigned long)rx,
+		.len = len,
+		.delay_usecs = 20,
+		.speed_hz = speed,
+		.bits_per_word = bits,
+	};
+*/
+	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+	if (ret < 1)
+		pabort("can't send spi message");
+
+	if (verbose)
+		hex_dump(tx, len, 32, "TX");
+
+	if (verbose)
+		hex_dump(rx, len, 32, "RX");
+//	sleep(0.01);
+
+//	ret = ioctl(fd, SPI_IOC_MESSAGE(0), NULL);
+//	if (ret < 1)
+//		pabort("can't send spi message");
+
+
+//	close(fd);
 }
 
-int main(int argc, char **argv)
+static void print_usage(const char *prog)
 {
-	const char *ubus_socket = NULL;
-	int ch;
+	printf("Usage: %s [-DsbdlHOLC3vpNR24SI]\n", prog);
+	puts("  -D --device   device to use (default /dev/spidev1.1)\n"
+	     "  -s --speed    max speed (Hz)\n"
+	     "  -d --delay    delay (usec)\n"
+	     "  -b --bpw      bits per word\n"
+	     "  -l --loop     loopback\n"
+	     "  -H --cpha     clock phase\n"
+	     "  -O --cpol     clock polarity\n"
+	     "  -L --lsb      least significant bit first\n"
+	     "  -C --cs-high  chip select active high\n"
+	     "  -3 --3wire    SI/SO signals shared\n"
+	     "  -v --verbose  Verbose (show tx buffer)\n"
+	     "  -p            Send data (e.g. \"1234\\xde\\xad\")\n"
+	     "  -N --no-cs    no chip select\n"
+	     "  -R --ready    slave pulls low to pause\n"
+	     "  -I --iter     iterations\n");
+	exit(1);
+}
 
-	while ((ch = getopt(argc, argv, "cs:")) != -1) {
-		switch (ch) {
+static void parse_opts(int argc, char *argv[])
+{
+	while (1) {
+		static const struct option lopts[] = {
+			{ "device",  1, 0, 'D' },
+			{ "speed",   1, 0, 's' },
+			{ "delay",   1, 0, 'd' },
+			{ "bpw",     1, 0, 'b' },
+			{ "loop",    0, 0, 'l' },
+			{ "cpha",    0, 0, 'H' },
+			{ "cpol",    0, 0, 'O' },
+			{ "lsb",     0, 0, 'L' },
+			{ "cs-high", 0, 0, 'C' },
+			{ "3wire",   0, 0, '3' },
+			{ "no-cs",   0, 0, 'N' },
+			{ "ready",   0, 0, 'R' },
+			{ "verbose", 0, 0, 'v' },
+			{ "iter",    1, 0, 'I' },
+			{ NULL, 0, 0, 0 },
+		};
+		int c;
+
+		c = getopt_long(argc, argv, "D:s:d:b:lHOLC3NRp:vI:",
+				lopts, NULL);
+
+		if (c == -1)
+			break;
+
+		switch (c) {
+		case 'D':
+			device = optarg;
+			break;
 		case 's':
-			ubus_socket = optarg;
+			speed = atoi(optarg);
+			break;
+		case 'd':
+			delay = atoi(optarg);
+			break;
+		case 'b':
+			bits = atoi(optarg);
+			break;
+		case 'l':
+			mode |= SPI_LOOP;
+			break;
+		case 'H':
+			mode |= SPI_CPHA;
+			break;
+		case 'O':
+			mode |= SPI_CPOL;
+			break;
+		case 'L':
+			mode |= SPI_LSB_FIRST;
+			break;
+		case 'C':
+			mode |= SPI_CS_HIGH;
+			break;
+		case '3':
+			mode |= SPI_3WIRE;
+			break;
+		case 'N':
+			mode |= SPI_NO_CS;
+			break;
+		case 'v':
+			verbose = 1;
+			break;
+		case 'R':
+			mode |= SPI_READY;
+			break;
+		case 'p':
+			input_tx = optarg;
+			break;
+		case 'I':
+			iterations = atoi(optarg);
 			break;
 		default:
+			print_usage(argv[0]);
 			break;
 		}
 	}
+}
 
-	uloop_init();
-	signal(SIGPIPE, SIG_IGN);
+static void transfer_escaped_string(int fd, char *str)
+{
+	size_t size = strlen(str);
+	uint8_t *tx;
+	uint8_t *rx;
 
-	ctx = ubus_connect(ubus_socket);
-	if (!ctx) {
-		fprintf(stderr, "Failed to connect to ubus\n");
-		return -1;
+	tx = malloc(size);
+	if (!tx)
+		pabort("can't allocate tx buffer");
+
+	rx = malloc(size);
+	if (!rx)
+		pabort("can't allocate rx buffer");
+
+	size = unescape((char *)tx, str, size);
+	transfer(fd, tx, rx, size);
+	free(rx);
+	free(tx);
+}
+void print_file(void)
+{
+	FILE* fd2;
+	fd2 = fopen("/sys/kernel/debug/gpio", "r");
+	if (fd2 < 0)
+		pabort("can't open /sys/kernel/debug/gpio");
+
+	int c; // note: int, not char, required to handle EOF
+	while ((c = fgetc(fd2)) != EOF) // standard C I/O file reading loop
+	putchar(c);
+
+	fclose(fd2);
+
+};
+int main(int argc, char *argv[])
+{
+	int ret = 0;
+	int fd;
+
+	parse_opts(argc, argv);
+
+	fd = open(device, O_RDWR);
+	if (fd < 0)
+		pabort("can't open device");
+
+
+	// spi mode
+	//mode = 0;
+
+	ret = ioctl(fd, SPI_IOC_WR_MODE32, &mode);
+	if (ret == -1)
+		pabort("can't set spi mode");
+
+	ret = ioctl(fd, SPI_IOC_RD_MODE32, &mode);
+	if (ret == -1)
+		pabort("can't get spi mode");
+
+	// bits per word
+	ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
+	if (ret == -1)
+		pabort("can't set bits per word");
+
+	ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
+	if (ret == -1)
+		pabort("can't get bits per word");
+
+	// max speed hz
+	ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+	if (ret == -1)
+		pabort("can't set max speed hz");
+
+	ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
+	if (ret == -1)
+		pabort("can't get max speed hz");
+
+	printf("spi mode: 0x%x bits per word: %d max speed: %d kHz\n", mode, bits, speed/1000);
+
+	if (input_tx){
+		transfer_escaped_string(fd, input_tx);
+	} else {
+		default_tx[0] = TABLE_READ;
+		//bits = 8;
+		transfer(fd, default_tx, default_rx, 1);
+        //print_file();
+        sleep(0.01);
+        //print_file();
+		memset(default_tx, 0x00, 4);
+		default_tx[0] = TABLE_READ_CONTINUE;
+		//bits = 8;
+		transfer(fd, default_tx, (uint8_t *)&my_ble_id_table, 4);
+        sleep(0.01);
+		SWAP_16(my_ble_id_table.size);
+		SWAP_16(my_ble_id_table.count);
+		//printf("my_ble_id_table[0,1,2,3]: 0x%2x 0x%2x 0x%2x 0x%2x\n", (uint8_t *)&my_ble_id_table, (uint8_t *)&my_ble_id_table+1, (uint8_t *)&my_ble_id_table+2, (uint8_t *)&my_ble_id_table+3);
+		printf("size: %d count: %d\n", my_ble_id_table.size , my_ble_id_table.count);
+		if (my_ble_id_table.count > 32) my_ble_id_table.count = 0;
+		while (my_ble_id_table.count > 0){
+			memset(default_tx, 0x00, 10);
+			default_tx[0] = TABLE_READ_CONTINUE;
+		    bits = 8;
+			transfer(fd, default_tx, (uint8_t *)&my_ble_id_table.cells, 10);
+            sleep(0.01);
+
+			printf("ID: %02X:%02X:%02X:%02X:%02X:%02X tx_pow:"\
+				, my_ble_id_table.cells[0].addr[5], my_ble_id_table.cells[0].addr[4], my_ble_id_table.cells[0].addr[3]\
+				, my_ble_id_table.cells[0].addr[2], my_ble_id_table.cells[0].addr[1], my_ble_id_table.cells[0].addr[0]);
+			if(my_ble_id_table.cells[0].tx_power_level == -127){
+				printf("  ?     ");
+			}else{
+                printf("%3d dBm ",my_ble_id_table.cells[0].tx_power_level);
+            
+            }
+			switch (my_ble_id_table.cells[0].m_data_0_dev_type){
+			    case 1:
+				    printf("bat:%3d%% rec:%3d type: ArmA M1.2\n", my_ble_id_table.cells[0].m_data_1_bat_lvl, my_ble_id_table.cells[0].m_data_2_rec_flag);
+			        break;
+			    case 2:
+				    printf("bat:%3d%% rec:%3d type: ArmA M1.3\n", my_ble_id_table.cells[0].m_data_1_bat_lvl, my_ble_id_table.cells[0].m_data_2_rec_flag);
+			        break;
+			    case 3:
+                    printf("bat:%3d%% rec:%3d type: ArmA M1.6\n", my_ble_id_table.cells[0].m_data_1_bat_lvl, my_ble_id_table.cells[0].m_data_2_rec_flag);
+                    break;
+			    default:
+                    printf("                 type: unknown\n");
+                    break;
+			};
+
+			my_ble_id_table.count--;
+		};
 	}
 
-	ubus_add_uloop(ctx);
+	close(fd);
 
-	server_main();
-
-	ubus_free(ctx);
-	uloop_done();
-
-	return 0;
+	return ret;
 }
